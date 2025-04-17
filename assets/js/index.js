b4cd981d61086bcc237fe31a688b480a6c4bba4a
janvipatel7 <janvi72004@gmail.com>
2025-04-17 17:42:51 +0530

    all done!!!!!

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,61 @@
 let checkbtn = document.getElementById("checkbtn");
-let ansReport = document.getElementById("ans");
+let stateName = document.getElementById("stateName");
+let totalCases = document.getElementById("totalCase");
+let deaths = document.getElementById("deaths");
+let recovered = document.getElementById("recovered");
+let clearbtn = document.getElementById("clearbtn");
 
 checkbtn.addEventListener("click", () => {
-    let stateName = document.getElementById("searchName").value.trim();
-
+    let stateName = document.getElementById("stateName").value.trim();
     const URL = "https://api.rootnet.in/covid19-in/stats/latest";
 
+    if(stateName === ""){
+        Swal.fire({
+            icon: "warning",
+            text: "State Not Found",
+            position: 'bottom-start',
+            toast: 'true',
+            showConfirmButton: false,
+            timer: 1500,
+        });
+        document.querySelector("#stateName").value = "";
+        return;
+    }
+
     fetch(URL)
-    .then((res) => res.json())
-    .then((data) => {
-        let states = data.data.regional;
-        console.log(states);
-        for (let i = 0; i < states.length; i++) {
-            if (states[i].loc.toLowerCase() === stateName.toLowerCase()) {
-                ansReport.innerHTML = 
-                    "State: " + states[i].loc + "<br>" +
-                    "Total Confirmed: " + states[i].totalConfirmed + "<br>" +
-                    "Discharged: " + states[i].discharged + "<br>" +
-                    "Deaths: " + states[i].deaths + "<br>";
+        .then((res) => res.json())
+        .then((data) => {
+            let states = data.data.regional;
+            let found = false;
+            console.log(states);
+            
+            for (let i = 0; i < states.length; i++) {
+                if (states[i].loc.toLowerCase() === stateName.toLowerCase()) {
+                    totalCase.innerHTML = states[i].totalConfirmed;
+                    deaths.innerHTML = states[i].deaths;
+                    recovered.innerHTML = states[i].discharged;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                Swal.fire({
+                    icon: "warning",
+                    text: "State Not Found",
+                    position: 'bottom-start',
+                    toast: 'true',
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
+                document.querySelector("#stateName").value = "";
                 return;
             }
-        }
-
-        ansReport.innerHTML = "State Not Found! Please Check Name....";
-    });
+        })
 });
+
+clearbtn.addEventListener("click", ()=>{
+    document.getElementById("stateName").value = "";
+    document.getElementById("totalCase").innerHTML = "0";
+    document.getElementById("deaths").innerHTML = "0";
+    document.getElementById("recovered").innerHTML = "0";
+})
